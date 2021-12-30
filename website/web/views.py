f9d59b07cb84beb0b74501fa4baaa59c223a4348
Doron <chapnitskydoron@gmail.com>
2021-12-30 18:00:58 +0200

    update

--- website/web/views.py
+++ website/web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
 # Create your views here.
+
+def home(request):
+    print(f'{os.getcwd()}')
+    return render(request, f'{os.getcwd()}\htmls\homelogin.html')
+    # return HttpResponse('<h1>FUCK YOU</h1>')
--- website/website/urls.py
+++ website/website/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import home
+# from website import homelogin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
