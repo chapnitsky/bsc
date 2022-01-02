b90fd00ee3483d2a11f365114779c47bf41d68aa
Doron Chapnitsky <69970498+chapnitsky@users.noreply.github.com>
2022-01-02 21:31:47 +0200

    updated count documents

--- webapi/web/views.py
+++ webapi/web/views.py
@@ -7,16 +7,17 @@ import pymongo as mongo
 # Create your views here.
 
 def home(request):
-    cli = mongo.MongoClient("mongodb://localhost:27017/")
-    db = cli.database
-    coll = db.data
-    res = coll.count_documents({})
-    print(res)
+
     # coll.insert_one({"_id": 0, "name": "fuck"})
-    print(f'{os.getcwd()}\web\htmls\homelogin.html\n')
+    # print(f'{os.getcwd()}\web\htmls\homelogin.html\n')
     return render(request, f'{os.getcwd()}\web\htmls\homelogin.html')
     # return HttpResponse('<h1>FUCK YOU</h1>')
 
 
 def sub(request):
+    cli = mongo.MongoClient("mongodb://localhost:27017/")
+    db = cli.database
+    coll = db.data
+    res = coll.count_documents({})
+    print(f'counted: {res}')
     return render(request, f'{os.getcwd()}\web\htmls\home.html')
