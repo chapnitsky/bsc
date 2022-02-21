83601e821f140be60cdc3c9f96ba86ba1d6fcb24
Doron <chapnitskydoron@gmail.com>
2022-02-21 17:16:20 +0200

    fixed unused django import

--- neural_networks.py
+++ neural_networks.py
@@ -1,7 +1,6 @@
 import json
 import tensorflow as tf
 import numpy as np
-import django as dj
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
