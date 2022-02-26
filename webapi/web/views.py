c8829519477c425b55b96e0b277ad8a4e282b19d
Doron <chapnitskydoron@gmail.com>
2022-02-26 20:38:17 +0200

    Update views.py
    login, home & generate from mongoDB

--- webapi/web/views.py
+++ webapi/web/views.py
@@ -2,31 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 import pymongo as mongo
-
+from collections import namedtuple
 
 cli = mongo.MongoClient("mongodb://localhost:27017/")
 db = cli.database
 coll = db.data
 
 
-def home(request):
+def login(request):
     # coll.insert_one({"_id": 0, "name": "fss"})
     # print(f'{os.getcwd()}\web\htmls\homelogin.html\n')
-    print("homed\n")
+    print("homed")
 
-    return render(request, f'{os.getcwd()}\web\htmls\homelogin.html')
+    return render(request, 'web/login.html', {"Title": "Login", "act": "/sub", "h1_base": "Login"})
 
 
 def checker(request):
     res = coll.find({})
+    unchecked = []
     for x in res:
-        print(f'document: {x}\n')
+        y = x
+        y['id'] = y.pop('_id')
+        if y['checked']:
+            continue
+        # y.pop('_id', None)  # For mongo o
+        object_name = namedtuple("aaa", y.keys())(*y.values())
 
-    return render(request, f'{os.getcwd()}\web\htmls\home.html')
+        print(type(object_name))
+        print(f'document: {object_name}\n')
+        unchecked.append(object_name)
+
+    return render(request, 'web/home.html', {"Title": "Home", "act": "", "h1_base": "Home", "sentences": unchecked})
 
 
-def sub(request):
+def home(request):
     res = coll.count_documents({})
     print(f'counted: {res}')
 
-    return render(request, f'{os.getcwd()}\web\htmls\home.html')
+    return render(request, 'web/home.html', {"Title": "Home", "act": "/checker", "h1_base": "Home"})
