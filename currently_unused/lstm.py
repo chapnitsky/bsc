de2816ff411189f629fa4f2fc7f7f4c2148a48a7
Doron <chapnitskydoron@gmail.com>
2022-05-07 18:59:48 +0300

    Update lstm.py

--- currently_unused/lstm.py
+++ currently_unused/lstm.py
@@ -1,9 +1,69 @@
 import torch
 import torch.nn.functional as F
 import torchtext
+import spacy
 import time
 import random
 import pandas as pd
+
+
+class RNN(torch.nn.Module):
+
+    def __init__(self, input_dim, embedding_dim, hidden_dim, output_dim):
+        super().__init__()
+
+        self.embedding = torch.nn.Embedding(input_dim, embedding_dim)
+        # self.rnn = torch.nn.RNN(embedding_dim,
+        #                        hidden_dim,
+        #                        nonlinearity='relu')
+        self.rnn = torch.nn.LSTM(embedding_dim,
+                                 hidden_dim)
+
+        self.fc = torch.nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, text):
+        # text dim: [sentence length, batch size]
+
+        embedded = self.embedding(text)
+        # embedded dim: [sentence length, batch size, embedding dim]
+
+        output, (hidden, cell) = self.rnn(embedded)
+        # output dim: [sentence length, batch size, hidden dim]
+        # hidden dim: [1, batch size, hidden dim]
+
+        hidden.squeeze_(0)
+        # hidden dim: [batch size, hidden dim]
+
+        output = self.fc(hidden)
+        return output
+
+
+def compute_accuracy(model, data_loader, device):
+    with torch.no_grad():
+        correct_pred, num_examples = 0, 0
+
+        for i, (features, targets) in enumerate(data_loader):
+            features = features.to(device)
+            targets = targets.float().to(device)
+
+            logits = model(features)
+            _, predicted_labels = torch.max(logits, 1)
+
+            num_examples += targets.size(0)
+            correct_pred += (predicted_labels == targets).sum()
+    return correct_pred.float() / num_examples * 100
+
+
+def predict_def(model, sentence):
+    model.eval()
+    tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+    indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+    length = [len(indexed)]
+    tensor = torch.LongTensor(indexed).to(DEVICE)
+    tensor = tensor.unsqueeze(1)
+    length_tensor = torch.LongTensor(length)
+    prediction = torch.nn.functional.softmax(model(tensor), dim=1)
+    return prediction[0][0].item()
 
 
 RANDOM_SEED = 58
@@ -18,12 +78,73 @@ EMBEDDING_DIM = 128
 HIDDEN_DIM = 256
 NUM_CLASSES = 2  # Default or not
 
-
 df = pd.read_csv('defaults.csv')
 TEXT = torchtext.legacy.data.Field(tokenize='spacy', tokenizer_language='en_core_web_sm')
-
 
 LABEL = torchtext.legacy.data.LabelField(dtype=torch.long)
 fields = [('TEXT_COLUMN_NAME', TEXT), ('LABEL_COLUMN_NAME', LABEL)]
 
 dataset = torchtext.legacy.data.TabularDataset(path='defaults.csv', format='csv', skip_header=True, fields=fields)
+train_data, test_data = dataset.split(split_ratio=[0.8, 0.2], random_state=random.seed(RANDOM_SEED))
+train_data, valid_data = train_data.split(split_ratio=[0.85, 0.15], random_state=random.seed(RANDOM_SEED))
+TEXT.build_vocab(train_data, max_size=VOCABULARY_SIZE)
+LABEL.build_vocab(train_data)
+train_loader, valid_loader, test_loader = \
+    torchtext.legacy.data.BucketIterator.splits(
+        (train_data, valid_data, test_data),
+        batch_size=BATCH_SIZE,
+        sort_within_batch=False,
+        sort_key=lambda x: len(x.TEXT_COLUMN_NAME),
+        device=DEVICE
+    )
+
+torch.manual_seed(RANDOM_SEED)
+model = RNN(input_dim=len(TEXT.vocab),
+            embedding_dim=EMBEDDING_DIM,
+            hidden_dim=HIDDEN_DIM,
+            output_dim=NUM_CLASSES  # could use 1 for binary classification
+            )
+
+model = model.to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+
+start_time = time.time()
+
+for epoch in range(NUM_EPOCHS):
+    model.train()
+    for batch_idx, batch_data in enumerate(train_loader):
+
+        text = batch_data.TEXT_COLUMN_NAME.to(DEVICE)
+        labels = batch_data.LABEL_COLUMN_NAME.to(DEVICE)
+
+        ### FORWARD AND BACK PROP
+        logits = model(text)
+        loss = F.cross_entropy(logits, labels)
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        ### UPDATE MODEL PARAMETERS
+        optimizer.step()
+
+        ### LOGGING
+        if not batch_idx % 50:
+            print(f'Epoch: {epoch + 1:03d}/{NUM_EPOCHS:03d} | '
+                  f'Batch {batch_idx:03d}/{len(train_loader):03d} | '
+                  f'Loss: {loss:.4f}')
+
+    with torch.set_grad_enabled(False):
+        print(f'training accuracy: '
+              f'{compute_accuracy(model, train_loader, DEVICE):.2f}%'
+              f'\nvalid accuracy: '
+              f'{compute_accuracy(model, valid_loader, DEVICE):.2f}%')
+
+    print(f'Time elapsed: {(time.time() - start_time) / 60:.2f} min')
+
+print(f'Total Training Time: {(time.time() - start_time) / 60:.2f} min')
+print(f'Test accuracy: {compute_accuracy(model, test_loader, DEVICE):.2f}%')
+
+nlp = spacy.blank("en")
+
+print('Probability positive:')
+predict_def(model, "This is such an awesome movie, I really love it!")
